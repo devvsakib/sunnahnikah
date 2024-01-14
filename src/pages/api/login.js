d5de31529990e68f2da16fc3bd32c435b3e1aa62
DevvSakib <devvsakib@gmail.com>
2024-01-14 23:04:28 +0600

    error fix

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,7 +1,4 @@
 // pages/api/login.js
-
-import { loginUser } from '../../path-to-your-backend-controller';
-
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         await loginUser(req, res);
